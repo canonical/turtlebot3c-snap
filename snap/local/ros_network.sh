351c9c88aed2cfb560702a576d78981c95d91b34
Guillaume Beuzeboc <guillaume.beuzeboc@gmail.com>
2023-05-22 10:38:48 +0200

    Update snap/local/ros_network.sh
    Co-authored-by: Jeremie Deray <jeremie.deray@canonical.com>

--- snap/local/ros_network.sh
+++ snap/local/ros_network.sh
@@ -2,7 +2,9 @@
 
 ROS_MASTER_HOST="$(snapctl get ros-master-host)"
 
-export ROS_MASTER_URI=http://${ROS_MASTER_HOST}:11311
+if [[ -z "${ROS_MASTER_URI}" ]]; then
+    export ROS_MASTER_URI=http://${ROS_MASTER_HOST}:11311
+fi
 export ROS_IP=${ROS_MASTER_HOST}
 
 exec $@
