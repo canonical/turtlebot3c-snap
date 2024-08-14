0f94c646ab0f284d9cafabc7f13d5d1c4c721c7c
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-08-14 14:44:01 +0200

    fix(firmware_hash): update function name

--- turtlebot3c-bringup-snap/snap/local/firmware_hash.sh
+++ turtlebot3c-bringup-snap/snap/local/firmware_hash.sh
@@ -27,7 +27,7 @@ write_firmware_hash() {
   echo "${firmware_hash}" > "${SNAP_COMMON}/OPENCR_HASH"
 }
 
-is_hash_the_same() {
+is_same_hash() {
   local firmware_hash=$(hash_firmware_directory)
   logger -t "${SNAP_NAME}" "OpenCR directory hash is ${firmware_hash}"
   local flashed_hash=$(cat "${SNAP_COMMON}/OPENCR_HASH")
