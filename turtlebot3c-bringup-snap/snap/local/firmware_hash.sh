c517de82723dda136dc86ac8e5cf29bd2d3e4b66
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-08-14 14:42:25 +0200

    fix(sh): use quotes and curly brackets everywhere

--- turtlebot3c-bringup-snap/snap/local/firmware_hash.sh
+++ turtlebot3c-bringup-snap/snap/local/firmware_hash.sh
@@ -1,42 +1,42 @@
 #!/bin/sh
 
 hash_file() {
-  sha256sum "$1" | awk '{print $1}'
+  sha256sum "${1}" | awk '{print $1}'
 }
 
 hash_directory() {
-  local dir="$1"
+  local dir="${1}"
   local hash=""
 
-  find "$dir" -type f -print0 | while IFS= read -r -d '' file; do
-    file_hash=$(hash_file "$file")
-    hash="$hash$file_hash"
+  find "${dir}" -type f -print0 | while IFS= read -r -d '' file; do
+    file_hash=$(hash_file "${file}")
+    hash="${hash}${file_hash}"
   done
 
   # Calculate final hash of concatenated file hashes
-  echo "$hash" | sha256sum | awk '{print $1}'
+  echo "${hash}" | sha256sum | awk '{print $1}'
 }
 
 hash_firmware_directory() {
-  hash_directory "$SNAP/opencr"
+  hash_directory "${SNAP}/opencr"
 }
 
 write_firmware_hash() {
   local firmware_hash=$(hash_firmware_directory)
-  logger -t $SNAP_NAME "OpenCR directory hash is $firmware_hash"
-  echo "${firmware_hash}" > "$SNAP_COMMON/OPENCR_HASH"
+  logger -t "${SNAP_NAME}" "OpenCR directory hash is ${firmware_hash}"
+  echo "${firmware_hash}" > "${SNAP_COMMON}/OPENCR_HASH"
 }
 
 is_hash_the_same() {
   local firmware_hash=$(hash_firmware_directory)
-  logger -t $SNAP_NAME "OpenCR directory hash is $firmware_hash"
-  local flashed_hash=$(cat "$SNAP_COMMON/OPENCR_HASH")
-  logger -t $SNAP_NAME "Flashed OpenCR directory hash is $flashed_hash"
-  if [ "$firmware_hash" = "$flashed_hash" ]; then
-    logger -t $SNAP_NAME "Same hashes!"
+  logger -t "${SNAP_NAME}" "OpenCR directory hash is ${firmware_hash}"
+  local flashed_hash=$(cat "${SNAP_COMMON}/OPENCR_HASH")
+  logger -t "${SNAP_NAME}" "Flashed OpenCR directory hash is ${flashed_hash}"
+  if [ "${firmware_hash}" = "${flashed_hash}" ]; then
+    logger -t "${SNAP_NAME}" "Same hashes!"
     return 0
   fi
-  logger -t $SNAP_NAME "Different hashes!"
+  logger -t "${SNAP_NAME}" "Different hashes!"
   return 1
 }
 
--- turtlebot3c-bringup-snap/snap/local/update_firmware.sh
+++ turtlebot3c-bringup-snap/snap/local/update_firmware.sh
@@ -2,7 +2,7 @@
 
 arch=$(uname -m)
 
-if [ "$arch" = "x86_64" ]; then
+if [ "${arch}" = "x86_64" ]; then
   echo "System is AMD64, only ARM devices can update the firmware."
   exit 1
 fi
@@ -11,15 +11,15 @@ OPENCR_PORT=$(snapctl get opencr-port)
 echo "OpenCR port is: ${OPENCR_PORT}"
 
 OPENCR_MODEL=$(snapctl get turtlebot3-model)
-if [ "$OPENCR_MODEL" = "waffle_pi" ]; then
+if [ "${OPENCR_MODEL}" = "waffle_pi" ]; then
   OPENCR_MODEL="waffle"
 fi
 echo "OpenCR model is: ${OPENCR_MODEL}"
 
-cd $SNAP/opencr
+cd ${SNAP}/opencr
 # The script always returns zero even in case of an error
-./update.sh "$OPENCR_PORT" "./$OPENCR_MODEL.opencr" | grep -q "Fail"
-if [ $? -eq 0 ]; then
+./update.sh "${OPENCR_PORT}" "./${OPENCR_MODEL}.opencr" | grep -q "Fail"
+if [ ${?} -eq 0 ]; then
   return 1
 fi
 
