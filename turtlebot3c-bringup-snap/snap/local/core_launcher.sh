2efe1cb8982c9fb60b0ec67f78d0593cb9dfb612
artivis <jeremie.deray@canonical.com>
2024-10-01 11:06:06 +0200

    add simu support
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- turtlebot3c-bringup-snap/snap/local/core_launcher.sh
+++ turtlebot3c-bringup-snap/snap/local/core_launcher.sh
@@ -7,4 +7,13 @@ if [[ -z "${TURTLEBOT3_MODEL}" ]]; then
     export TURTLEBOT3_MODEL="$(snapctl get turtlebot3-model)"
 fi
 
-ros2 launch turtlebot3_bringup robot.launch.py
+SIMULATION="$(snapctl get simulation)"
+
+case ${SIMULATION} in
+  false|0)
+    ros2 launch turtlebot3_bringup robot.launch.py
+    ;;
+  true|1)
+    ros2 launch turtlebot3_bringup turtlebot3_state_publisher.launch.py simulation:="${SIMULATION}"
+    ;;
+esac
